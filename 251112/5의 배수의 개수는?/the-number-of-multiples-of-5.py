c471826f21c3eb25b32a2a1f6ce22868a76543b6
Lee Sung Jae <sa478@naver.com>
2025-11-12 16:48:06 +0900

    Title: 5의 배수의 개수는?, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251112/5의 배수의 개수는?/the-number-of-multiples-of-5.py
@@ -0,0 +1,12 @@
+cnt=0;
+arr = [
+    list(map(int, input().split()))
+    for _ in range(4)
+]
+
+for i in range(4):
+    for j in range(4):
+        if arr[i][j]%5==0:
+            cnt+=1
+
+print(cnt)
