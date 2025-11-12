0cb3c469268509a7cef973aa64ea995da5ec68c1
Lee Sung Jae <sa478@naver.com>
2025-11-12 16:57:49 +0900

    Title: 좌우로 반복하며 출력, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251112/좌우로 반복하며 출력/print-in-left-right-reverse.py
@@ -0,0 +1,10 @@
+a= int(input())
+
+for i in range(a):
+    if i%2==0:
+        for j in range(1, a+1):
+            print(j,end="")
+    else:
+        for j in range(a, 0, -1):
+            print(j,end="")
+    print()
