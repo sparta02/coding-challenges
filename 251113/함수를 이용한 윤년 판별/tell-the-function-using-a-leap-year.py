d21f291a3d3a7330309f56ba10445290ee48d1e2
Lee Sung Jae <sa478@naver.com>
2025-11-13 19:19:11 +0900

    Title: 함수를 이용한 윤년 판별, Time: 80ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 251113/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -0,0 +1,12 @@
+y = int(input())
+
+# Please write your code here.
+
+def chk(n):
+    if n%4==0:
+        return "true"
+    elif n%100==0 and a%400!=0
+        return "true"
+    return "false"
+
+print(chk(n))
