1d0aec9496805daa2dc443a14b96116ed3195bb1
Lee Sung Jae <sa478@naver.com>
2025-11-14 00:02:40 +0900

    Title: 나누고 빼면서 합하기, Time: 81ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251114/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+
+# Please write your code here.
+sum=A[m-1]
+
+while(m!=1):
+    if m%2==0:
+        m=int(m/2)
+    else:
+        M-=1
+    sum+=A[m-1]
+
+print(sum)
