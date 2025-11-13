4622b51dd4227ee10e81ecd411f74e543215de66
Lee Sung Jae <sa478@naver.com>
2025-11-13 19:20:49 +0900

    Title: 함수를 이용한 윤년 판별, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251113/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 251113/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -3,10 +3,10 @@ y = int(input())
 # Please write your code here.
 
 def chk(n):
-    if n%4==0:
+    if n%4==0 and n%400!=0:
         return "true"
-    elif n%100==0 and a%400!=0:
-        return "false"
-    return "true"
+    elif n%100==0 :
+        return "true"
+    return "false"
 
 print(chk(y))
