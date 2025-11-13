969a85fde376f0b4ba496ebcc6ea4f4e932a2ef1
Lee Sung Jae <sa478@naver.com>
2025-11-13 20:02:09 +0900

    Title: 사칙연산 함수, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251113/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,19 @@
+a, o, c = input().split()
+a = int(a)
+c = int(c)
+
+# Please write your code here.
+def check(a, o, c):
+    if o=='+':
+        return a+c
+    elif o=='-':
+        return a-c
+    elif o=='/':
+        return a//c
+    elif o=='*':
+        return a*c
+    else:
+        return False
+if check(a,o,c)==False:
+    print(False)
+print(f"{a} {o} {c} = {check(a,o,c)}")
