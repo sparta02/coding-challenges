e360003f572540552967a7302203817a6ec5a497
Lee Sung Jae <sa478@naver.com>
2025-11-13 20:09:01 +0900

    Title: 함수를 이용한 온전수 판별, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251113/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
+++ 251113/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -2,17 +2,16 @@ a, b = map(int, input().split())
 
 # Please write your code here.
 def check(n):
-    if a%2==0:
+    if n%2==0:
         return False
-    elif a%10==5:
+    elif n%10==5:
         return False
-    elif a%3==0 and a%9!=0:
+    elif n%3==0 and n%9!=0:
         return False
     return True
 
 cnt=0
 for i in range(a, b+1):
-    print(i, check(i))
     if check(i):
         cnt+=1
 print(cnt)
