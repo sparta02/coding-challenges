30eb094eee5f65e2201de791bc1d86df3294d410
Lee Sung Jae <sa478@naver.com>
2025-11-13 20:13:31 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 53ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251113/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
@@ -0,0 +1,18 @@
+a, b = map(int, input().split())
+
+# Please write your code here.
+def check(n):
+    for i in range(2,n):
+        if n%i==0:
+            return False
+    if sum(list(map(int, str(n))))%2!=0:
+        return False
+    return True
+
+cnt=0
+for i in range(a, b+1):
+    print(i, check(i))
+    if check(i):
+        cnt+=1
+
+print(cnt)
