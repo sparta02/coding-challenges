6557e80b663beb3db24f379fc6f177a5f7cd89e9
Lee Sung Jae <sa478@naver.com>
2025-11-15 15:30:53 +0900

    Title: 총점 비교, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251115/총점 비교/compare-total-points.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+
+arr=[tuple(input().split()) for _ in range(n)]
+
+arr.sort(key=lambda x: int(x[1])+int(x[2])+int(x[3]))
+for i in range(n):
+    print(arr[i][0],arr[i][1],arr[i][2],arr[i][3])
+
+
+# Please write your code here.
