639691a9530b23fb6561865c867e3f98b919209c
Lee Sung Jae <sa478@naver.com>
2025-11-15 13:48:01 +0900

    Title: 007, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251115/007/007.py
@@ -0,0 +1,16 @@
+secret_code, meeting_point, time = input().split()
+time = int(time)
+
+# Please write your code here.
+
+class why:
+    def __init__(self, a, b, c):
+        self.a=a
+        self.b=b
+        self.c=c
+
+
+hi=why(secret_code, meeting_point, time)
+print(f"secret code : {hi.a}")
+print(f"meeting point : {hi.b}")
+print(f"time : {hi.c}")
