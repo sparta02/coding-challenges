df6f1a47019585aad87752926d72359f3494fa18
Lee Sung Jae <sa478@naver.com>
2025-11-15 19:03:06 +0900

    Title: 흰검 칠하기, Time: 130ms, Memory: 28MB, Status: Runtime Error - Codetree

--- 251115/흰검 칠하기/painting-white-black.py
+++ 251115/흰검 칠하기/painting-white-black.py
@@ -8,7 +8,7 @@ for num, direction in commands:
 
 # Please write your code here.
 
-arr=[[0,0,-1] for i in range(10000)]
+arr=[[0,0,-1] for i in range(100002)]
 현재=int(len(arr)/2)
 def cal(steps, way):
     global arr
