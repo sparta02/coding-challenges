40b735d3ae7b4279130c8e58230bc17b267ddd75
Lee Sung Jae <sa478@naver.com>
2025-11-15 17:22:13 +0900

    Title: 그 요일은, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251115/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,27 @@
+m1, d1, m2, d2 = map(int, input().split())
+A = input()
+
+
+arr=[0,31,28,31,30,31,30,31,31,30,31,30,31]
+
+def calc(m, d):
+    cnt=0
+    a=1
+    b=1
+    while True:
+        if a==m and b==d:
+            return cnt
+        
+        cnt+=1
+        b+=1
+
+        if arr[a]<b:
+            a+=1
+            b=1
+
+
+day=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+
+idx = (calc(m2, d2)-calc(m1, d1))//7
+
+print(idx+1)
