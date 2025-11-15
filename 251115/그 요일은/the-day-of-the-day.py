2c51087d2f73a086aa1565feb9a358aca841dc7e
Lee Sung Jae <sa478@naver.com>
2025-11-15 17:25:43 +0900

    Title: 그 요일은, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 251115/그 요일은/the-day-of-the-day.py
+++ 251115/그 요일은/the-day-of-the-day.py
@@ -18,10 +18,19 @@ def calc(m, d):
         if arr[a]<b:
             a+=1
             b=1
-
+5
+12
+19
+26
+4
+11
 
 day=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
 
 idx = (calc(m2, d2)-calc(m1, d1))//7
+차이=(calc(m2, d2)-calc(m1, d1))%7
+if 차이 >= day.index(A):
+    idx+=1
 
-print(idx+1)
+
+print(idx)
