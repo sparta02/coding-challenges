681916aa73e3dfad2f361b89d47e807761489593
Lee Sung Jae <sa478@naver.com>
2025-11-16 14:30:28 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 50ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 251116/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 251116/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -13,15 +13,25 @@ handshakes.sort(key=lambda x: x[0])
 
 def 감염(i, j):
     global people
+    temp_a=0
+    temp_b=0
     if people[i][0]==1 and people[i][1]<K:
-        people[j][0]=1
-        people[i][1]+=1
+            temp_a=1
     if people[j][0]==1 and people[j][1]<K:
+            temp_b=1
+    
+    if temp_a==1:
+        people[j][0]=1
+        people[j][1]=0
+        people[i][1]+=1
+    if temp_b==1:
         people[i][0]=1
+        people[i][1]=0
         people[j][1]+=1
 
 for hand in handshakes:
     감염(hand[1], hand[2])
+
 
 for i in range(1, N+1):
     print(people[i][0], end="")
