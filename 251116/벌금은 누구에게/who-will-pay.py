af0620b585c490c62fd1b696f6b6d9954a6f26fa
Lee Sung Jae <sa478@naver.com>
2025-11-16 13:37:39 +0900

    Title: 벌금은 누구에게, Time: 74ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 251116/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,13 @@
+N, M, K = map(int, input().split())
+student = [int(input()) for _ in range(M)]
+
+# Please write your code here.
+
+arr=[0]*(N+1)
+
+for stu in student:
+    arr[stu]+=1
+
+    if arr[stu]>=K:
+        print(stu)
+        break
