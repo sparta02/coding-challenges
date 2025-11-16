2f70745c56c465a9634d2043e2e1af0a490fb0b8
Lee Sung Jae <sa478@naver.com>
2025-11-16 13:34:17 +0900

    Title: 만나는 그 순간, Time: 109ms, Memory: 28MB, Status: Passed - Codetree

--- 251116/만나는 그 순간/the-moment-we-meet.py
+++ 251116/만나는 그 순간/the-moment-we-meet.py
@@ -16,20 +16,41 @@ for _ in range(m):
 
 # Please write your code here.
 
-result=-1
-cnt=0
+
 a=[]
 a_idx=0
 b=[]
 b_idx=0
 for i in range(n):
-    if d=='R':
-        for j in range(t):
+    if d[i]=='R':
+        for j in range(t[i]):
             a_idx+=1
             a.append(a_idx)
     else:
-        for j in range(t):
+        for j in range(t[i]):
             a_idx-=1
             a.append(a_idx)
-print(a)
 
+for i in range(m):
+    if d2[i]=='R':
+        for j in range(t2[i]):
+            b_idx+=1
+            b.append(b_idx)
+    else:
+        for j in range(t2[i]):
+            b_idx-=1
+            b.append(b_idx)
+
+
+result=-1
+cnt=0
+
+for i in range(max(len(a),len(b))):
+    if a[i]==b[i]:
+        result=i+1
+        break
+
+print(result)
+
+
+    
