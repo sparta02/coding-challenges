4a85b192bee1370046ba9691126d37011d1c92e3
Lee Sung Jae <sa478@naver.com>
2025-11-16 13:17:10 +0900

    Title: 연속되는 수 3, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251116/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+# Please write your code here.
+result=0
+temp=0
+for i in range(len(arr)): 
+    if i==0 or (abs(arr[i])+abs(arr[i-1])==abs(arr[i]+arr[i-1])):
+        temp+=1
+        if result<temp:
+            result=temp
+    else:
+        temp=1
+    
+
+print(result)
