650d749f39679516767b999fc28d04155d7f61f8
Lee Sung Jae <sa478@naver.com>
2025-11-16 14:50:07 +0900

    Title: 문자에 따른 명령 2, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251116/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,19 @@
+dirs = input()
+
+# Please write your code here.
+way=0
+dx, dy= [0,1,0,-1], [1,0,-1,0]
+x=0
+y=0
+
+for i in dirs:
+    if i=="L":
+        way=(way+1)%4
+    elif i=="R":
+        way=(way+3)%4
+    elif i=="F":
+        x=x+dx[way]
+        y=y+dy[way]
+
+print(x, y)
+
