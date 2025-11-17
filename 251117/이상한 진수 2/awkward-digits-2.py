ec0d2d4ba30fbdb77c1bb844f89fcac2fc9c1420
Lee Sung Jae <sa478@naver.com>
2025-11-17 23:56:01 +0900

    Title: 이상한 진수 2, Time: 84ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 251117/이상한 진수 2/awkward-digits-2.py
+++ 251117/이상한 진수 2/awkward-digits-2.py
@@ -1,9 +1,10 @@
 a = input()
 
 # Please write your code here.
-a=a.replace("0","1",1)
-
+b=a.replace("0","1",1)
+if a==b:
+    b=b-1
 sum=0
-for i in range(len(a)):
-    sum= sum*2 +int(a[i])
+for i in range(len(b)):
+    sum= sum*2 +int(b[i])
 print(sum)
