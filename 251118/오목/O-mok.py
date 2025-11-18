4ed7aa666d85506471d780e28c16cd056d83f440
Lee Sung Jae <sa478@naver.com>
2025-11-18 13:11:03 +0900

    Title: 오목, Time: 52ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251118/오목/O-mok.py
@@ -0,0 +1,32 @@
+board = [list(map(int, input().split())) for _ in range(19)]
+
+# Please write your code here.
+
+def check_y(x,y):
+    standard=board[x][y]
+    if board[x][y+1]==standard and board[x][y+2]==standard and board[x][y+3]==standard and board[x][y+4]==standard:
+        return True
+    return False
+
+def check_x(x,y):
+    standard=board[x][y]
+    if board[x+1][y]==standard and board[x+2][y]==standard and board[x+3][y]==standard and board[x+4][y]==standard:
+        return True
+    return False
+result=0
+result_x=0
+result_y=0
+for x in range(15):
+    for y in range(19):
+        if check_x(x,y) and (board[x][y]==1 or board[x][y]==2):
+            result=board[x][y]
+            result_x=x+3
+            result_y=y+1
+for x in range(19):
+    for y in range(15):
+        if check_y(x,y) and (board[x][y]==1 or board[x][y]==2):
+            result=board[x][y]
+            result_x=x+1
+            result_y=y+3
+print(result)
+print(result_x, result_y)
