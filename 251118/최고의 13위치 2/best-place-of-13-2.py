61bc155c1691709e9e4b8282b37fa1892c7c3442
Lee Sung Jae <sa478@naver.com>
2025-11-18 14:07:06 +0900

    Title: 최고의 13위치 2, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251118/최고의 13위치 2/best-place-of-13-2.py
+++ 251118/최고의 13위치 2/best-place-of-13-2.py
@@ -1,20 +1,19 @@
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
-arr=[[0]* for _ in range(3)]
 # Please write your code here.
-
-max1=-1
-standard_i=-1
-standard_j=-1
+max1=0
+standard_i=0
+standard_j=0
 for i in range(N):
     for j in range(N-2):
         if arr[i][j]+arr[i][j+1]+arr[i][j+2]>max1:
             max1=arr[i][j]+arr[i][j+1]+arr[i][j+2]
             standard_i=i
             standard_j=j
-arr[standard_i][standard_j]=-9
-arr[standard_i][standard_j+1]=-9
-arr[standard_i][standard_j+2]=-9
+if max1!=0:
+    arr[standard_i][standard_j]=-9
+    arr[standard_i][standard_j+1]=-9
+    arr[standard_i][standard_j+2]=-9
 
 max2=-1
 #print(arr)
