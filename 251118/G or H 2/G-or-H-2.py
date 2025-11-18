10680b8a5e2d3b517522a0378e8f4c98af4b48ef
Lee Sung Jae <sa478@naver.com>
2025-11-18 20:14:38 +0900

    Title: G or H 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251118/G or H 2/G-or-H-2.py
+++ 251118/G or H 2/G-or-H-2.py
@@ -21,7 +21,7 @@ for i in range(len(people)):
             elif people[k][1]=="H":
                 temp_H+=1
         #print(people[i:j+1], temp_G, temp_H, people[j][0],people[i][0])
-        if temp_G==temp_H:
+        if temp_G==temp_H or temp_G==0 or temp_H==0:
             temp=people[j][0]-people[i][0]
         result=max(result,temp)
 
