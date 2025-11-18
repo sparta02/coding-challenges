26cc2658bcf1e64ed7249ab19bc182fd9e5625eb
Lee Sung Jae <sa478@naver.com>
2025-11-18 20:23:18 +0900

    Title: 밭의 높이를 고르게하기, Time: 87ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 251118/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -0,0 +1,16 @@
+N, H, T = map(int, input().split())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+result=999999999999999999999999999999999999999
+
+for i in range(N-H+1):
+    #temp_arr=arr[i:i+T]
+    print(arr[i:i+T])
+    temp=0
+    for j in range(T):
+        temp+=abs(H-temp_arr[j])
+    #print(temp)
+    result=min(result, temp)
+
+print(result)
