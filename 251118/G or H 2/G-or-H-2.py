163c8f8d3a84e7995adac1d627787f034b16685e
Lee Sung Jae <sa478@naver.com>
2025-11-18 20:15:20 +0900

    Title: G or H 2, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 251118/G or H 2/G-or-H-2.py
+++ 251118/G or H 2/G-or-H-2.py
@@ -1,33 +1,28 @@
 n = int(input())
 people = [list(input().split()) for _ in range(n)]
-for i in range(len(people)):
-    people[i][0]=int(people[i][0])
+for i in range(n):
+    people[i][0] = int(people[i][0])
 
-
-# Please write your code here.
-
+# 위치 기준 정렬
 people.sort(key=lambda x: x[0])
 
-result=0
+result = 0
 
-for i in range(len(people)):
-    for j in range(i+1, len(people)):
-        temp_G=0
-        temp_H=0
-        temp=0
-        for k in range(i, j+1):
-            if people[k][1]=="G":
-                temp_G+=1
-            elif people[k][1]=="H":
-                temp_H+=1
-        #print(people[i:j+1], temp_G, temp_H, people[j][0],people[i][0])
-        if temp_G==temp_H or temp_G==0 or temp_H==0:
-            temp=people[j][0]-people[i][0]
-        result=max(result,temp)
+for i in range(n):
+    g = 0
+    h = 0
+    for j in range(i, n):
+        if people[j][1] == "G":
+            g += 1
+        else:
+            h += 1
 
-if len(people)==1:
-    result=0
+        # 전부 G이거나 전부 H
+        if h == 0 or g == 0:
+            result = max(result, people[j][0] - people[i][0])
+
+        # G == H
+        if g == h:
+            result = max(result, people[j][0] - people[i][0])
 
 print(result)
-
-
