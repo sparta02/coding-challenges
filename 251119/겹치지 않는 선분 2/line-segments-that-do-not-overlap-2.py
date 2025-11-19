10bb8cdb0361dba082d9199d460728639e4cf722
Lee Sung Jae <sa478@naver.com>
2025-11-19 18:41:27 +0900

    Title: 겹치지 않는 선분 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251119/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 251119/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -4,12 +4,11 @@ result=[0]*n
 
 # Please write your code here.
 
-cnt=0
 for i in range(n):
     for j in range(n):
         if i==j:
             continue
-        if lines[i][0]<lines[j][0]<lines[i][1] and lines[i][0]<lines[j][1]<lines[i][1]:
+        if (lines[i][0]<lines[j][0] and lines[j][1]<lines[i][1]) or (lines[i][0]>lines[j][0] and lines[j][1]>lines[i][1]):
             result[i]=1
             result[j]=1
 
