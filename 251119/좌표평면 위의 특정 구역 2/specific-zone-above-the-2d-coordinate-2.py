485d6ee33af6dc16889c2cc466793f4ef1b90ad1
Lee Sung Jae <sa478@naver.com>
2025-11-19 14:07:26 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 82ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 251119/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -0,0 +1,24 @@
+n = int(input())
+points = [tuple(map(int, input().split())) for _ in range(n)]
+x = [p[0] for p in points]
+y = [p[1] for p in points]
+
+# Please write your code here.
+
+result=9999999999999999999
+
+for i in range(n):
+    min_x=99999
+    mix_y=99999
+    max_x=0
+    max_y=0
+    for j in range(n):
+        if i==j:
+            continue
+        min_x=min(min_x, x[j])
+        min_y=min(min_y, y[j])
+        max_x=max(max_x, x[j])
+        max_y=max(max_y, y[j])
+
+print((max_x-min_x)*(max_y-min_y))
+        
