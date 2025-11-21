5519a31e6ddafb4ebec7b0308400ce36d7117fa9
Lee Sung Jae <sa478@naver.com>
2025-11-21 17:44:57 +0900

    Title: 독서실의 거리두기 5, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251121/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 251121/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -2,4 +2,14 @@ N = int(input())
 seat = input()
 
 # Please write your code here.
-print(seat.find("1"))
+arr=[]
+result=seat.find("1")
+#print(result)
+끝에서2번째인덱스=0
+for i in range(seat.count("1")-2):
+    #print(seat.find("1", seat.find("1", i-1)+1), seat.find("1", seat.find("1", i)+1))
+    result=max(result, int((seat.find("1", seat.find("1", i)+1)-seat.find("1", seat.find("1", i-1)+1))/2))
+    끝에서2번째인덱스=seat.find("1", seat.find("1", i)+1)
+    #print(int((seat.find("1", seat.find("1", i-1)+1)-seat.find("1", seat.find("1", i)+1))/2), result)
+result=max(result, len(seat)-seat.find("1", 끝에서2번째인덱스+1)-1)
+print(result)
