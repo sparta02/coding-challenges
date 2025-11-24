2d2081602747b19ced7ef8cdc8bce594b3822425
Lee Sung Jae <sa478@naver.com>
2025-11-24 16:38:16 +0900

    Title: 수들의 최대 차, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251124/수들의 최대 차/maximum-difference-in-numbers.py
+++ 251124/수들의 최대 차/maximum-difference-in-numbers.py
@@ -5,20 +5,17 @@ arr = [int(input()) for _ in range(N)]
 
 # Please write your code here.
 
-result=0 # 반복문을 돌며 갱신하여 최종적으로 출력할 수
+arr.sort()
 
-check_flag=-1
-for i in range(N,0,-1): # N~1개를 고르기
-    #print(i)
-    temp_arr=list(combinations(arr, i))
+result=0
 
-    for item in temp_arr: # 임시로 만든 temp_arr 내의 list들 체크
-        max_num=max(item)
-        min_num=min(item)
-        if max_num-min_num<=K: # 최대값 - 최소값이 K 이하라면
-            print(len(item))
-            check_flag=1
+for i in range(N):
+    count=1
+    for j in range(i+1, N):
+        if j-i<=K:
+            count+=1
+        else:
             break
-    if check_flag==1:
-        break
-        
+    result=max(result, count)
+
+print(result)
