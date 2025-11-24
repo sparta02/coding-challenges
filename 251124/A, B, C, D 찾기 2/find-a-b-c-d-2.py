36c555f8ea3dcd742c9ba947ccd491e2f2ee3ec4
Lee Sung Jae <sa478@naver.com>
2025-11-24 17:26:12 +0900

    Title: A, B, C, D 찾기 2, Time: 79ms, Memory: 17MB, Status: Passed - Codetree

--- 251124/A, B, C, D 찾기 2/find-a-b-c-d-2.py
+++ 251124/A, B, C, D 찾기 2/find-a-b-c-d-2.py
@@ -1,6 +1,7 @@
 nums = list(map(int, input().split()))
 
 # Please write your code here.
+nums.sort()
 check=0
 for i in range(1, 40):
     for j in range(i, 40):
@@ -8,10 +9,26 @@ for i in range(1, 40):
             for l in range(k, 40):
                 temp_arr=[]
                 temp_arr.append(i)
-                temp_arr.append(i)
-                temp_arr.append(i)
-                temp_arr.append(i)
-                temp_arr.append(i)
+                temp_arr.append(j)
+                temp_arr.append(k)
+                temp_arr.append(l)
+                temp_arr.append(i+j)
+                temp_arr.append(i+k)
+                temp_arr.append(i+l)
+                temp_arr.append(j+k)
+                temp_arr.append(j+l)
+                temp_arr.append(k+l)
+                temp_arr.append(i+j+k)
+                temp_arr.append(i+j+l)
+                temp_arr.append(i+k+l)
+                temp_arr.append(j+k+l)
+                temp_arr.append(i+j+k+l)
+                temp_arr.sort()
+                #print(temp_arr, nums)
+                if nums==temp_arr:
+                    print(i,j,k,l)
+                    check=1
+                    break
             if check:
                 break
         if check:
