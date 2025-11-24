ce123b3657fcb51d1a89a71092b1dbefa064235c
Lee Sung Jae <sa478@naver.com>
2025-11-24 16:56:35 +0900

    Title: 훌륭한 점프, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251124/훌륭한 점프/great-jump.py
@@ -0,0 +1,27 @@
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+def check_stone(check_num):
+    temp_arr=[]
+    for i, n in enumerate(arr):
+        if n<=check_num:
+            temp_arr.append(i)
+    #print(check_num,temp_arr)
+    if 0 not in temp_arr or (n-1) not in temp_arr:
+        return False
+    for i in range(len(temp_arr)-1):
+        dist = temp_arr[i+1] - temp_arr[i]
+        if dist>k:
+            return False
+    return True
+
+
+
+result=999
+
+for i in range(n, min(arr[0], arr[-1])-1,-1):
+    if check_stone(i):
+        result=min(result, i)
+
+print(result)
