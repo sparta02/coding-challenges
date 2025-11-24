29f29cae918bf9f1426b0308003ad63840d980bf
Lee Sung Jae <sa478@naver.com>
2025-11-24 18:37:34 +0900

    Title: 초기 수열 복원하기, Time: 68ms, Memory: 17MB, Status: Passed - Codetree

--- 251124/초기 수열 복원하기/restore-initial-sequence.py
+++ 251124/초기 수열 복원하기/restore-initial-sequence.py
@@ -1,17 +1,25 @@
-from itertools import permutations
-
 n = int(input())
 adjacent = list(map(int, input().split()))
 
 # Please write your code here.
+arr=[i for i in range(1, n+1)]
 
-temp_arr=[i for i in range(1, n+1)]
-arr=list(permutations(temp_arr, n))
+for i in range(1, n+1):
+    temp_arr=[]
+    temp_arr.append(i)
 
-for items in arr:
-    for i in range(n-1):
-        if items[i]+items[i+1]!=adjacent[i]:
+    check_list=[0]*(n+1)
+    check_list[i]=1
+    #print(temp_arr, check_list)
+    for j in range(n-1):
+        #print(adjacent[j], temp_arr[-1])
+        next_num=adjacent[j]-temp_arr[-1]
+        #print(temp_arr, check_list, next_num)
+        if next_num > n or next_num<1 or check_list[next_num]==1:
             break
-    else:
-        for j in range(n):
-            print(items[j], end=" ")
+        else:
+            check_list[next_num]=1
+            temp_arr.append(next_num)
+            if len(temp_arr)==n:
+                for k in range(n):
+                    print(temp_arr[k], end=" ")
