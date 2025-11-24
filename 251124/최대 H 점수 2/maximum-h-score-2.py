abc9ffb4f48a76ba5b9b8cdd4db64d628abb3265
Lee Sung Jae <sa478@naver.com>
2025-11-24 21:45:22 +0900

    Title: 최대 H 점수 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251124/최대 H 점수 2/maximum-h-score-2.py
+++ 251124/최대 H 점수 2/maximum-h-score-2.py
@@ -1,32 +1,27 @@
-from itertools import combinations
-
 N, L = map(int, input().split())
 arr = list(map(int, input().split()))
 
-def h_index(a):
-    a.sort(reverse=True)
-    h = 0
-    for i in range(len(a)):
-        if a[i] >= i + 1:
-            h = i + 1
-        else:
-            break
-    return h
+max_h = 0
 
-# L이 0이면 바로 H-index 계산
-if L == 0:
-    print(h_index(arr[:]))
-    exit()
+# 가능한 모든 H 값을 확인 (0부터 N까지)
+for h in range(N + 1):
+    # 현재 h 이상인 원소 개수
+    count_ge_h = sum(1 for x in arr if x >= h)
+    
+    # 이미 조건을 만족하면
+    if count_ge_h >= h:
+        max_h = max(max_h, h)
+        continue
+    
+    # 부족한 개수
+    needed = h - count_ge_h
+    
+    # h 미만인 원소들 중에서 h-1인 원소 개수
+    # (이들을 1 증가시키면 h가 됨)
+    count_h_minus_1 = sum(1 for x in arr if x == h - 1)
+    
+    # h-1인 원소가 충분하고, L 범위 내라면 가능
+    if count_h_minus_1 >= needed and needed <= L:
+        max_h = max(max_h, h)
 
-result = 0
-
-# 0개부터 L개까지 선택해서 모두 탐색
-for k in range(0, L + 1):
-    for comb in combinations(range(N), k):
-        temp = arr[:]
-        for idx in comb:
-            temp[idx] += 1
-        
-        result = max(result, h_index(temp))
-
-print(result)
+print(max_h)
