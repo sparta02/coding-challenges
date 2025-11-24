a773843e690493a75069793d03bdc7f921029697
Lee Sung Jae <sa478@naver.com>
2025-11-24 16:42:33 +0900

    Title: 수들의 최대 차, Time: 71ms, Memory: 17MB, Status: Passed - Codetree

--- 251124/수들의 최대 차/maximum-difference-in-numbers.py
+++ 251124/수들의 최대 차/maximum-difference-in-numbers.py
@@ -8,9 +8,10 @@ arr.sort()
 result=0
 
 for i in range(N):
-    count=1
-    for j in range(i+1, N):
-        if j-i<=K:
+    count=0
+    for j in range(i, N):
+        #print(i, j)
+        if arr[j]-arr[i]<=K:
             count+=1
         else:
             break
