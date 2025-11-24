6fc234ebf535651db9358780d4978dea798e74e8
Lee Sung Jae <sa478@naver.com>
2025-11-24 18:58:48 +0900

    Title: 이상한 폭탄 3, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251124/이상한 폭탄 3/strange-bomb-3.py
+++ 251124/이상한 폭탄 3/strange-bomb-3.py
@@ -22,10 +22,12 @@ count=0
 
 for i in result_arr:
     temp_count=result_arr.count(i)
-    if count <=temp_count and value<i and temp_count>1:
+    if count<temp_count and temp_count>1:
         value=i
         count=temp_count
-        
+    if count==temp_count and value<i and temp_count>1:
+        value=i
+        count=temp_count
 
 
 print(value)
