d194ea1f64e67998bdff16d62836d9a8b70c5147
Lee Sung Jae <sa478@naver.com>
2025-11-24 21:58:42 +0900

    Title: 두 선분, Time: 51ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251124/두 선분/two-lines.py
+++ 251124/두 선분/two-lines.py
@@ -2,7 +2,7 @@ x1, x2, x3, x4 = map(int, input().split())
 
 # Please write your code here.
 
-if x2<x3 or x4<x1:
+if x2<=x3 or x4<=x1:
     print("nonintersection")
 else:
     print("intersecting")
