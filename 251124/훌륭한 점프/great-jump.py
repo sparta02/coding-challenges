03033ae23675ddb7f8ea053bc0cc4793931def79
Lee Sung Jae <sa478@naver.com>
2025-11-24 17:02:13 +0900

    Title: 훌륭한 점프, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251124/훌륭한 점프/great-jump.py
+++ 251124/훌륭한 점프/great-jump.py
@@ -10,18 +10,21 @@ def check_stone(check_num):
     #print(check_num,temp_arr)
     if 0 not in temp_arr or (n-1) not in temp_arr:
         return False
-    for i in range(len(temp_arr)-1):
-        dist = temp_arr[i+1] - temp_arr[i]
-        #print(temp_arr[i+1], temp_arr[i], dist)
-        if dist>k:
+    # ★ prev를 0번 돌로 맞춰두고 거리 비교 시작
+    prev = 0
+
+    for idx in temp_arr:
+        if idx - prev > k:  # 다음 밟을 수 있는 돌까지 거리
             return False
+        prev = idx
+
     return True
 
 
 
 result=999
 
-for i in range(n, 0,-1):
+for i in range(n, min(arr[0], arr[-1])-1,-1):
     if check_stone(i):
         result=min(result, i)
 
