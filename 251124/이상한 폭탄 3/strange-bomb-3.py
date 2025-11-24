d99c6a3809314004590a7db6c9db69134b2cec51
Lee Sung Jae <sa478@naver.com>
2025-11-24 19:13:20 +0900

    Title: 이상한 폭탄 3, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251124/이상한 폭탄 3/strange-bomb-3.py
+++ 251124/이상한 폭탄 3/strange-bomb-3.py
@@ -1,35 +1,27 @@
 N, K = map(int, input().split())
 num = [int(input()) for _ in range(N)]
 
-# Please write your code here.
-
-def check_index(n):
-    if n<0 or n>=N:
-        return False
-    return True
-
-result_arr=[]
+explode_count = {}
 
 for i in range(N):
-    for j in range(i-K, i+K+1):
-        if check_index(j) and num[i]==num[j]:
-            result_arr.append(num[i])
-            #print(num[i])
+    cur = num[i]
+    exploded = False
+
+    # 현재 폭탄 기준 거리 K 범위 확인
+    for j in range(max(0, i-K), min(N, i+K+1)):
+        if i != j and num[j] == cur:
+            exploded = True
             break
 
-value=0
-count=0
+    # 폭발한다면 카운트 증가
+    if exploded:
+        explode_count[cur] = explode_count.get(cur, 0) + 1
 
-for i in result_arr:
-    temp_count=num.count(i)
-    if count<temp_count:
-        value=i
-        count=temp_count
-    if count==temp_count and value<i:
-        value=i
-        count=temp_count
-
-
-print(value)
-
-        
+# 아무 폭탄도 터지지 않았을 경우
+if not explode_count:
+    print(0)
+else:
+    # 가장 많이 터진 번호, 동점일 경우 번호가 큰 것 선택
+    max_count = max(explode_count.values())
+    candidates = [num for num, cnt in explode_count.items() if cnt == max_count]
+    print(max(candidates))
