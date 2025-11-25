360da902c893ea982de1d75b08cc58934d20ea59
Lee Sung Jae <sa478@naver.com>
2025-11-25 17:05:51 +0900

    Title: 코딩톡, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251125/코딩톡/coding-talk.py
+++ 251125/코딩톡/coding-talk.py
@@ -3,15 +3,20 @@ messages = [tuple(input().split()) for _ in range(m)]
 c = [msg[0] for msg in messages]
 u = [int(msg[1]) for msg in messages]
 
-# Please write your code here.
-arr=[ chr(65+i) for i in range(n)]
-#print(arr)
-arr.remove(c[p-1])
-for i in range(p, m):
-    if c[i] in arr:
-        arr.remove(c[i])
-    #print(c[i], u[i])
+people = [chr(ord('A') + i) for i in range(n)]
 
-if u[p-1]!=0:
-    for i in range(len(arr)):
-        print(arr[i], end=" ")
+# p번째 메시지를 보낸 사람
+sender_p = c[p-1]
+
+# p번째 이후에 메시지를 보낸 사람들 (읽었음이 확실한 사람)
+sure_read = set()
+for i in range(p, m):
+    sure_read.add(c[i])
+
+# p번째 메시지 보낸 사람도 읽었음이 확실
+sure_read.add(sender_p)
+
+# 읽지 않았을 가능성이 있는 사람
+possible = [x for x in people if x not in sure_read]
+
+print(*possible)
