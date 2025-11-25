e41bf1b38e7b086f3f2f5ea754337f0c5e9c9156
Lee Sung Jae <sa478@naver.com>
2025-11-25 17:30:11 +0900

    Title: 코딩톡, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251125/코딩톡/coding-talk.py
+++ 251125/코딩톡/coding-talk.py
@@ -1,35 +1,27 @@
-n, m, p = map(int, input().split())
-messages = [tuple(input().split()) for _ in range(m)]
+import sys
 
-# u 기준으로 정렬
-messages_sorted = sorted(
-    [(i, msg[0], int(msg[1])) for i, msg in enumerate(messages)],
-    key=lambda x: x[2]
-)
+n, m, p = tuple(map(int, input().split()))
+message = [
+    list(input().split())
+    for _ in range(m)
+]
 
-# 정렬 후 p가 어디로 갔는지 찾기
-original_p_index = p - 1
-for new_idx, (old_idx, c, u) in enumerate(messages_sorted):
-    if old_idx == original_p_index:
-        p_new = new_idx
-        break
+# 모두 읽은 채팅이라면 읽지 않은 사람은 없습니다.
+if int(message[p - 1][1]) == 0:
+    sys.exit()
 
-# 정렬된 리스트에서 다시 c, u 분리
-c = [item[1] for item in messages_sorted]
-u = [item[2] for item in messages_sorted]
+# 각 사람에 대해 채팅을 읽었을지 안 읽었을지 판단합니다.
+for i in range(n):
+    # read : 확실하게 채팅을 읽었으면 true
+    person = chr(ord('A') + i)
+    read = False
 
-arr = [chr(65 + i) for i in range(n)]
-
-# p 메시지 보낸 사람 제거
-if c[p_new] in arr:
-    arr.remove(c[p_new])
-
-# p 이후 메시지 보낸 사람 제거 (정렬 기반)
-for i in range(p_new + 1, m):
-    if c[i] in arr:
-        arr.remove(c[i])
-
-# u[p_new]가 0이 아닌 경우에만 출력
-if u[p_new] != 0:
-    for x in arr:
-        print(x, end=" ")
+    # 만약 p번 메시지를 읽은 사람 수와 같은 채팅을 기준으로
+    # 한번이라도 채팅을 쳤다면 확실하게 채팅을 읽었습니다.
+    for c, u in message:
+        u = int(u)
+        if u >= int(message[p - 1][1]) and c == person:
+            read = True
+    
+    if read == False:
+        print(person, end=" ")
