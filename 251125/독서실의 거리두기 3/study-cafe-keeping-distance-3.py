5997a3c016218a5f57b97aa1295cf3b9c4be0fdc
Lee Sung Jae <sa478@naver.com>
2025-11-25 18:49:23 +0900

    Title: 독서실의 거리두기 3, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251125/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
+++ 251125/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -3,36 +3,17 @@ seat = input()
 seats=[int(x) for x in seat]
 
 # Please write your code here.
+arr=[]
 left=0
-
-standard=0
-최대값=0
-최대_left=0
-최대_right=0
 for i in range(1, N):
     if seats[i]==1:
-        #print(i, left)
+        
         차이=i-left
-        if 차이>최대값:
-            최대값=차이
-            최대_left=left
-            최대_right=i
+        arr.append(차이)
         left=i
-       
-#print(최대값)
-#print(최대_left, 최대_right)
-seats[int((최대_left+최대_right)/2)]=1
 
-left=0
-
-standard=0
-최소값=9999999999
-최대_left=0
-최대_right=0
-for i in range(1, N):
-    if seats[i]==1:
-        #print(i, left)
-        차이=i-left
-        최소값=min(최소값, 차이)
-       
-print(최소값)
+arr.sort()
+temp=arr[-1]
+arr[-1]=int(arr[-1]/2)
+arr.append(temp-arr[-1])
+print(min(arr))
