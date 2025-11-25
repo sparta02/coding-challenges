3277fbe1e0d3df87ec59e25a25bfdd84166fe3d5
Lee Sung Jae <sa478@naver.com>
2025-11-25 17:00:51 +0900

    Title: 코딩톡, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251125/코딩톡/coding-talk.py
@@ -0,0 +1,16 @@
+n, m, p = map(int, input().split())
+messages = [tuple(input().split()) for _ in range(m)]
+c = [msg[0] for msg in messages]
+u = [int(msg[1]) for msg in messages]
+
+# Please write your code here.
+arr=[ chr(65+i) for i in range(n)]
+#print(arr)
+arr.remove(c[p-1])
+for i in range(p, m):
+    if c[i] in arr:
+        arr.remove(c[i])
+    #print(c[i], u[i])
+
+for i in range(len(arr)):
+    print(arr[i], end=" ")
