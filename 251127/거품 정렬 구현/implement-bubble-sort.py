5c704c6a6d96b2c698418be9aab1d5fc2bafa578
Lee Sung Jae <sa478@naver.com>
2025-11-27 21:52:56 +0900

    Title: 거품 정렬 구현, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251127/거품 정렬 구현/implement-bubble-sort.py
+++ 251127/거품 정렬 구현/implement-bubble-sort.py
@@ -6,7 +6,7 @@ for i in range(n-1):
     for j in range(0,n-i-1):
         if arr[j]>arr[j+1]:
             arr[j], arr[j+1]= arr[j+1], arr[j]
-    print(arr)
+    #print(arr)
 
-for i in range(n):
+for i in arr:
     print(i, end=" ")
