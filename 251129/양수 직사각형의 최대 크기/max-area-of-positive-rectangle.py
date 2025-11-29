2572bca1d45a8a69c2c633c6f6c135539b395b2f
Lee Sung Jae <sa478@naver.com>
2025-11-29 21:00:58 +0900

    Title: 양수 직사각형의 최대 크기, Time: 99ms, Memory: 19MB, Status: Passed - Codetree

--- 251129/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 251129/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -12,11 +12,12 @@ for x in range(1, n+1): # 세로 길이
             for j in range(0, m-y+1): # x축 검사 시작 위치
             # x가 2, y가 2라면
             # i는 0~2까지, j는 0~3까지 돌면서 확인
+                #print(f"x:{x}, y:{y}, i:{i}, j:{j}")
                 check=1
                 for x_check in range(i, i+x):
                     for y_check in range(j, j+y):
                         # 만약 음수가 발견되면 check를 0으로 설정
-                        if grid[x_check][y_check]<0:
+                        if grid[x_check][y_check]<=0:
                             check=0
                 # 모든 요소가 양수라면
                 if check:
