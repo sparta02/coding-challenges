f2ad9872a1e2923d8028e959d15a9b3baae146cb
Lee Sung Jae <sa478@naver.com>
2025-12-01 12:09:45 +0900

    Title: ABC 줄 세우기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251201/ABC 줄 세우기/abc-line-up.py
+++ 251201/ABC 줄 세우기/abc-line-up.py
@@ -4,13 +4,11 @@ arr=list(input().split())
 # Please write your code here.
 result=0
 
-for i in range(n):
-    result+=abs(ord(arr[i]) - (i+65))
-
-print(int(result/2))
-
-
-# 1 4 3 2
-# 1 2 3 4
-
-# 0 2 0 2
+for i in range(n-1):
+    if arr[i]==chr(65+i):
+        continue
+    실제위치 = arr.index(chr(65+i))
+    for j in range(실제위치, i,-1):
+        arr[j], arr[j-1] = arr[j-1], arr[j]
+        result+=1
+print(result)
