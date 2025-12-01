36965f36bb1d2478e2f014cbb77688c8503976dc
Lee Sung Jae <sa478@naver.com>
2025-12-01 14:28:32 +0900

    Title: 세 수의 최대 곱, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251201/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 251201/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -1,15 +1,36 @@
-from itertools import combinations
-
 n = int(input())
 arr = list(map(int, input().split()))
 
 # Please write your code here.
 
-comb = list(combinations(arr,3))
+arr.sort()
+arr_minus=[ item for item in arr if item<0 ]
+arr_plus=[ item for item in arr if item>0 ]
+# print(arr_minus)
+# print(arr_plus)
+result= -999999999999999999999999999999
+# Case1: 양수 3개
+# 1, 2, 3, 4
+if len(arr_plus)>=3:
+    result=max(result, arr_plus[-1]*arr_plus[-2]*arr_plus[-3])
+# Case2: 양수 2개, 음수 1개
+# -2, -1, 3, 4
+if len(arr_plus)>=2 and len(arr_minus)>=1:
+    result=max(result, arr_plus[-1]*arr_plus[-2]*arr_minus[-1])
 
-result=-99999999999999999999999999999999
+# Case3: 양수 1개, 음수 2개
+# -3, -2, -1, 5
+if len(arr_plus)>=1 and len(arr_minus)>=2:
+    result=max(result, arr_plus[-1]*arr_minus[0]*arr_minus[1])
 
-for nums in comb:
-    result=max(result, nums[0]*nums[1]*nums[2])
+
+# Case4: 음수 3개
+# -4, -3, -2, -1
+if len(arr_minus)>=3:
+    result=max(result, arr_minus[-1]*arr_minus[-2]*arr_minus[-3])
+
+# 모두 0과 비교
+if 0 in arr:
+    result=max(result,0)
 
 print(result)
