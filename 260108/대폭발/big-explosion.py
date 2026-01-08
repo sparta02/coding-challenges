34ce9e92a3aa7be2173c1e5e68736b29f378b700
Lee Sung Jae <sa478@naver.com>
2026-01-08 16:12:56 +0900

    Title: 대폭발, Time: 68ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 260108/대폭발/big-explosion.py
+++ 260108/대폭발/big-explosion.py
@@ -4,14 +4,8 @@ n, m, r, c = map(int, input().split())
 grid=[[0]*n for _ in range(n)]
 grid[r-1][c-1]=1
 
-def make_new_grid():
-    new_grid=[[0]*n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            new_grid[i][j]=grid[i][j]
-    return new_grid
 
-def print_grid(grid):
+def print_grid():
     for i in range(n):
         for j in range(n):
             print(grid[i][j], end=" ")
@@ -19,20 +13,25 @@ def print_grid(grid):
     print()
 
 for time in range(1, m+1):
-    new_grid=make_new_grid()
-    # print_grid(new_grid)
+
     for x in range(n):
         for y in range(n):
             
-            for i in range(n):
-                for j in range(n):
-                    if ((abs(x-i) ==time and y==j) or (abs(y-j) == time and x==i)) and grid[i][j]==1:
-                        new_grid[x][y]=1
+            if grid[x][y] != 1:
+                continue
+            dx=[0,1,0,-1]
+            dy=[1,0,-1,0]
 
+            for i in range(4):
+                if 0<=x+dx[i]*time<n and 0<=y+dy[i]*time<n and grid[x+dx[i]*time][y+dy[i]*time] !=1:
+                    grid[x+dx[i]*time][y+dy[i]*time]=2
+    # print_grid()
     for i in range(n):
         for j in range(n):
-            grid[i][j]=new_grid[i][j]
-    # print_grid(grid)
+            if grid[i][j] ==2:
+                grid[i][j]=1
+    # print_grid()
+
 
 result=0
 for i in range(n):
