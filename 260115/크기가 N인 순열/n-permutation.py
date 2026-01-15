9c67898425fe3c4b71a4f4d1046d005756bce596
Lee Sung Jae <sa478@naver.com>
2026-01-15 13:28:52 +0900

    Title: 크기가 N인 순열, Time: 132ms, Memory: 30MB, Status: Passed - Codetree

--- 260115/크기가 N인 순열/n-permutation.py
+++ 260115/크기가 N인 순열/n-permutation.py
@@ -1,33 +1,12 @@
+from itertools import permutations
+
 n = int(input())
-visited=[False]*(n+1)
+
 # Please write your code here.
 
-temp_list=[]
+result_list=list(permutations(range(1, n+1), n))
 
-def print_list():
-    for i in range(n):
-        print(temp_list[i], end=" ")
+for hi in result_list:
+    for j in range(n):
+        print(hi[j], end=" ")
     print()
-
-def plus(curr_num):
-    global visited
-    global temp_list
-    if curr_num == n:
-        print_list()
-        return
-
-    for i in range(1, n+1):
-        # print(visited)
-        # print(temp_list)
-        if visited[i]==True:
-            continue
-        
-        temp_list.append(i)
-        visited[i]=True
-
-        plus(curr_num+1)
-
-        temp_list.pop()
-        visited[i]=False
-
-plus(0)
