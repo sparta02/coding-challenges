6c587bd9de79a6ed2364cfd8a2698382bb740254
Lee Sung Jae <sa478@naver.com>
2026-01-17 14:39:13 +0900

    Title: 사각형 채우기 3, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260117/사각형 채우기 3/rectangle-fill-3.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+# Please write your code here.
+dp=[-1]*1005
+
+dp[1]=2
+dp[2]=7
+
+# Case 1. 1x2 사각형을 추가한다
+# dp[i-1] + 1
+
+# Case 2. 1x1 사각형을 2개 추가한다
+# dp[i-1] + 1
+
+# Case 3. 2x1 사각형을 2개 추가한다
+# dp[i-2] + 1
+
+for i in (3, n+1):
+    dp[i]=dp[i-1]*2+dp[i-2]
+
+print(dp[n])
