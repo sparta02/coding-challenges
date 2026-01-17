d52715b9e346a43a43d17ecf4fe8189663d42707
Lee Sung Jae <sa478@naver.com>
2026-01-17 14:33:59 +0900

    Title: 피보나치 수, Time: 77ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 260117/피보나치 수/fibonacci-number.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+# Please write your code here.
+
+
+memo =[-1]*50
+
+
+def fibo(num):
+    if memo[num]!=-1:
+        return memo[num]
+
+    if num==1:
+        memo[num]=1
+    elif num==2:
+        memo[num]=1
+    else:
+        memo[num]=fib(num-1)+fib(num-2])
+    #print(memo)
+    return memo[num]
+
+print(fibo(n))
