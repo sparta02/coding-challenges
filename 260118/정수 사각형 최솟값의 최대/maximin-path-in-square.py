f20b077c809eb221ec0c07b09f9e4222c6673887
Lee Sung Jae <sa478@naver.com>
2026-01-18 16:02:51 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260118/정수 사각형 최솟값의 최대/maximin-path-in-square.py
+++ 260118/정수 사각형 최솟값의 최대/maximin-path-in-square.py
@@ -5,26 +5,29 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 
 dp=[[0]*n for _ in range(n)]
 
-dp[0][0]=9
-dp[0][1] = grid[0][1]
-dp[1][0] = grid[1][0]
+if n==1:
+    print(dp[0][0])
+else:
+    dp[0][0]=9
+    dp[0][1] = grid[0][1]
+    dp[1][0] = grid[1][0]
 
-for i in range(1, n):
-    dp[0][i]=min(dp[0][i-1], grid[0][i])
+    for i in range(1, n):
+        dp[0][i]=min(dp[0][i-1], grid[0][i])
 
-for i in range(1, n):
-    dp[i][0]=min(dp[i-1][0], grid[i][0])
-print(dp)
+    for i in range(1, n):
+        dp[i][0]=min(dp[i-1][0], grid[i][0])
+    print(dp)
 
-for x in range(1, n):
-    for y in range(1, n):
-        dp[x][y]=min(max(dp[x-1][y], dp[x][y-1]),grid[x][y])
+    for x in range(1, n):
+        for y in range(1, n):
+            dp[x][y]=min(max(dp[x-1][y], dp[x][y-1]),grid[x][y])
 
 
-# for x in range(n):
-#     for y in range(n):
-#         print(dp[x][y], end=" ")
-#     print()
-# print()
+    # for x in range(n):
+    #     for y in range(n):
+    #         print(dp[x][y], end=" ")
+    #     print()
+    # print()
 
-print(dp[n-1][n-1])
+    print(dp[n-1][n-1])
