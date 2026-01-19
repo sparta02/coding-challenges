5de37e2f1eab8fbf1677a01de1f817cc82df7f06
Lee Sung Jae <sa478@naver.com>
2026-01-19 13:57:36 +0900

    Title: 최대 감소 부분 수열, Time: 57ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 260119/최대 감소 부분 수열/longest-decreasing-subsequence.py
@@ -0,0 +1,16 @@
+n = int(input())
+m = list(map(int, input().split()))
+
+#dp의 조건
+# j(앞에 있는 수)가 i(뒤에 있는 수)보다 작으면 된다
+
+dp=[1]*n
+dp[0]=1
+for i in range(1,n):
+    for j in range(i):
+        #print(f"i:{i} j:{j}")
+        if m[j]>m[i] and dp[j]+1>dp[i]:
+            dp[i]=dp[j]+1
+    #print(dp)
+
+print(max(dp))
