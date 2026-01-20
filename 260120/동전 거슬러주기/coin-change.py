3dcb676950969395ca4ee0964006e6578db7f28e
Lee Sung Jae <sa478@naver.com>
2026-01-20 13:27:39 +0900

    Title: 동전 거슬러주기, Time: 76ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 260120/동전 거슬러주기/coin-change.py
+++ 260120/동전 거슬러주기/coin-change.py
@@ -9,9 +9,10 @@ for i in range(1, m+1):
     for cost in coin:
         # i원을 만들고 싶을 때
         # cost 동전을 사용
-        if dp[i-cost]==999999:
-            continue
-        dp[i]=min(dp[i-cost]+1, dp[i])
+        if cost>=i
+            if dp[i-cost]==999999:
+                continue
+            dp[i]=min(dp[i-cost]+1, dp[i])
 
 if dp[-1]==999999:
     print(-1)
