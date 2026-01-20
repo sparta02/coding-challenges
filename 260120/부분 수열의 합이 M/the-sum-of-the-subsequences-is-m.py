99a559f163fa7fbcd6e6a0e2091fe534fb17e7bc
Lee Sung Jae <sa478@naver.com>
2026-01-20 13:40:18 +0900

    Title: 부분 수열의 합이 M, Time: 54ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 260120/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+
+# Please write your code here.
+dp=[-1]*(m+1)
+dp[0]=0
+
+for num in A:
+    #print(num)
+    # num 혼자 m을 초과해버리면 skip
+    if m>num:
+        # 기존에 가능한 모든 숫자들에 대해
+        for i in range(m, 1, -1):
+            if num>i:
+                continue
+            
+            if dp[i-num]==-1:
+                continue
+            #print(i, num)
+            dp[i]=dp[i-num]+1
+
+    #print(dp)
+
+print(dp[m])
