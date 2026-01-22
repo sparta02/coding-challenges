d8b9aaa69191a663be191646156c9b7fd42b6955
Lee Sung Jae <sa478@naver.com>
2026-01-22 11:27:33 +0900

    Title: 숫자 전쟁, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260122/숫자 전쟁/number-war.py
+++ 260122/숫자 전쟁/number-war.py
@@ -21,7 +21,10 @@ def print_grid():
 
 dp[0][0]=0
 for i in range(1, n+1):
-    dp[0][i]=0
+    if first_cards[i-1]<second_cards[0]:
+        dp[0][i]=0
+    else:
+        break
 
 for i in range(1, n+1):
     for j in range(n+1):
