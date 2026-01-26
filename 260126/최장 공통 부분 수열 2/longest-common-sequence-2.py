9b1b6743a89674d98810d0d340582b51b2b98514
Lee Sung Jae <sa478@naver.com>
2026-01-26 17:19:55 +0900

    Title: 최장 공통 부분 수열 2, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260126/최장 공통 부분 수열 2/longest-common-sequence-2.py
+++ 260126/최장 공통 부분 수열 2/longest-common-sequence-2.py
@@ -3,30 +3,29 @@ B = input()
 N=len(A)
 M=len(B)
 
-dp= [['']*M for _ in range(N)]
+dp= [[0]*M for _ in range(N)]
 
 if A[0] == B[0]:
-    dp[0][0]=A[0]
+    dp[0][0]=1
 
 for i in range(1, M):
     if A[0]==B[i]:
-        dp[0][i]=A[0]
+        dp[0][i]=1
     else:
         dp[0][i]=dp[0][i-1]
 
 for i in range(1, N):
     if A[i]==B[0]:
-        dp[i][0]=B[0]
+        dp[i][0]=1
     else:
         dp[i][0]=dp[i-1][0]
 
 for i in range(1, N):
     for j in range(1, M):
         if A[i]==B[j]:
-            if dp[i-1][j-1]=='':
-                dp[i][j]=A[i]
-            else:
-                dp[i][j]=dp[i-1][j-1]+A[i]
+            dp[i][j]=max(dp[i][j], dp[i-1][j-1]+1)
+        else:
+            dp[i][j]=max(dp[i][j], dp[i-1][j], dp[i][j-1])
 
 
 def print_dp():
@@ -42,4 +41,19 @@ def print_dp():
     print()
 
 # print_dp()
-print(dp[-1][-1])
+
+# 3. 역추적 (실제 문자열 찾기)
+result = []
+i, j = N, M
+while i > 0 and j > 0:
+    if A[i-1] == B[j-1]:
+        result.append(A[i-1])
+        i -= 1
+        j -= 1
+    else:
+        # 더 큰 값을 가진 쪽으로 이동
+        if dp[i-1][j] >= dp[i][j-1]:
+            i -= 1
+        else:
+            j -= 1
+print(result)
