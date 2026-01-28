8bdd587f0ec37ab5c80730c7abbe81afac8ab01f
Lee Sung Jae <sa478@naver.com>
2026-01-28 10:52:32 +0900

    Title: 자주 등장한 top K 숫자, Time: 263ms, Memory: 80MB, Status: Memory Limit Exceed - Codetree

--- 260128/자주 등장한 top K 숫자/top-k-frequent-elements.py
+++ 260128/자주 등장한 top K 숫자/top-k-frequent-elements.py
@@ -2,13 +2,13 @@ n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
 # Please write your code here.
-count_list=[0]*100001
+count_list=[0]*100000001
 
 for i in range(n):
     count_list[arr[i]]+=1
 
 new_list=[]
-for i in range(100001):
+for i in range(100000001):
     if count_list[i]!=0:
         new_list.append((i, count_list[i]))
 
