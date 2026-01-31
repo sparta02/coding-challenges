384287563c11e07c79be5b66a0bd6ff8b52b5902
Lee Sung Jae <sa478@naver.com>
2026-01-31 11:44:02 +0900

    Title: 마지막으로 남은 숫자, Time: 225ms, Memory: 27MB, Status: Wrong Answer - Codetree

---
+++ 260131/마지막으로 남은 숫자/last-remaining-number.py
@@ -0,0 +1,20 @@
+import heapq
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+
+pq=[]
+for num in arr:
+    heapq.heappush(pq, -num)
+
+while(len(pq)>1):
+    num1=-heapq.heappop(pq)
+    num2=-heapq.heappop(pq)
+    
+    if (num1-num2)!=0:
+        heapq.heappush(pq, abs(num1-num2))
+
+print(-1 if len(pq)==0 else pq[0])
+
