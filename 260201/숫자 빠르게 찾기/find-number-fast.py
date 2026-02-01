b341cff139a9e967becbe3f3e7a4563847d7c01c
Lee Sung Jae <sa478@naver.com>
2026-02-01 15:45:40 +0900

    Title: 숫자 빠르게 찾기, Time: 1099ms, Memory: 24MB, Status: Time Limit Exceed - Codetree

---
+++ 260201/숫자 빠르게 찾기/find-number-fast.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+queries = [int(input()) for _ in range(m)]
+
+# Please write your code here.
+
+def find_num(num):
+    left=0
+    right=n-1
+    while(left<=right):
+        mid= int(right+left)
+        if num<arr[mid]:
+            right=mid-1
+        elif num>arr[mid]:
+            left=mid+1
+        elif num==arr[mid]:
+            return mid+1
+    return -1
+
+# print(find_num(2))
+for num in queries:
+    print(find_num(num))
