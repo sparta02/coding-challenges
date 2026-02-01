4962d2a12bfdea84fa336b1c1d6b9ba4657b70fc
Lee Sung Jae <sa478@naver.com>
2026-02-01 16:04:38 +0900

    Title: 가장 먼저 나오는 숫자, Time: 78ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 260201/가장 먼저 나오는 숫자/first-appear-number.py
@@ -0,0 +1,27 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+query = list(map(int, input().split()))
+
+# Please write your code here.
+
+def lower(num):
+    left=0
+    right=n-1
+    lower_bound=n
+
+    while(left<=right):
+        mid = (left+right)//2
+        if num<=arr[mid]:
+            lower_bound=min(lower_bound,mid)
+            right=mid-1
+        else:
+            left=mid+1
+    return lower_bound
+
+
+for num in query:
+    lower_bound=lower(num)
+    if arr[lower_bound]==num:
+        print(lower_bound+1)
+    else:
+        print(-1)
