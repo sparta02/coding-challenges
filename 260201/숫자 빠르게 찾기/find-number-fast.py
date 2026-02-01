1b8f86ac76b240572dce651e5b012fa264002046
Lee Sung Jae <sa478@naver.com>
2026-02-01 15:50:26 +0900

    Title: 숫자 빠르게 찾기, Time: 205ms, Memory: 24MB, Status: Passed - Codetree

--- 260201/숫자 빠르게 찾기/find-number-fast.py
+++ 260201/숫자 빠르게 찾기/find-number-fast.py
@@ -8,7 +8,7 @@ def find_num(num):
     left=0
     right=n-1
     while(left<=right):
-        mid= int(right+left)
+        mid= int((right+left)/2)
         if num<arr[mid]:
             right=mid-1
         elif num>arr[mid]:
