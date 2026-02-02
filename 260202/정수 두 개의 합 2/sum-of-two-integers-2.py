1678d340626f7249db00a0c500932de5bc4e7bea
Lee Sung Jae <sa478@naver.com>
2026-02-02 11:36:25 +0900

    Title: 정수 두 개의 합 2, Time: 146ms, Memory: 21MB, Status: Wrong Answer - Codetree

---
+++ 260202/정수 두 개의 합 2/sum-of-two-integers-2.py
@@ -0,0 +1,33 @@
+n, k = map(int, input().split())
+arr = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+arr.sort()
+
+result=0
+
+def find_num(num):
+    #print(f"{num}이하인 수 중 가장 오른쪽")
+    left=0
+    right=n-1
+    min_index=-1
+
+    while(left<=right):
+        mid = (left+right)//2
+        if arr[mid]<=num:
+            min_index=max(min_index, mid)
+            left=mid+1
+        else:
+            right=mid-1
+    return min_index
+
+for i in range(n):
+    if arr[i]>=k:
+        break
+    # k-arr[i] 이하인 수들 중 가장 오른쪽에 있는 수
+    j=find_num(k-arr[i])
+    #print(j)
+    if j!=0:
+        result+=(j-i)
+
+print(result)
