3a9529e3e7c8d8044e5b4b16328347a6afb6cb7a
Lee Sung Jae <sa478@naver.com>
2026-02-03 11:54:26 +0900

    Title: 수 합치기, Time: 215ms, Memory: 23MB, Status: Passed - Codetree

--- 260203/수 합치기/merge-numbers.py
+++ 260203/수 합치기/merge-numbers.py
@@ -1,17 +1,20 @@
+import heapq
+
 n = int(input())
 arr = list(map(int, input().split()))
 
+
 # Please write your code here.
 result=0
+heapq.heapify(arr)
 
 while (len(arr)>1):
-    num1=min(arr)
-    arr.remove(num1)
-    num2=min(arr)
-    arr.remove(num2)
+    num1=heapq.heappop(arr)
+    num2=heapq.heappop(arr)
+
     temp_num=num1+num2
     
-    arr.append(temp_num)
+    heapq.heappush(arr, temp_num)
     result+=temp_num
     #print(arr)
 
