dab835afe8c4bcfa4dcc0664b26cb8ce43e52d43
Lee Sung Jae <sa478@naver.com>
2026-02-03 11:49:10 +0900

    Title: 1이 K개 이상 존재하는 부분 수열, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260203/1이 K개 이상 존재하는 부분 수열/subsequence-with-k-or-more-1s.py
@@ -0,0 +1,22 @@
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+
+one_list=[]
+
+for i in range(n):
+    if arr[i]==1:
+        one_list.append(i)
+
+#print(one_list)
+
+result=9999999
+
+if len(one_list)>k:
+    for i in range(len(one_list)-(k-1)):
+        result=min(result, one_list[i+k-1]-one_list[i]+1)
+else:
+    result=-1
+
+print(result)
