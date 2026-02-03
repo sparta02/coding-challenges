9a32eb7d58128044898468551c18a234a0d8edd2
Lee Sung Jae <sa478@naver.com>
2026-02-03 12:13:12 +0900

    Title: 최대 수 만들기, Time: 75ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 260203/최대 수 만들기/make-biggest-num.py
@@ -0,0 +1,19 @@
+from functools import cmp_to_key
+
+
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+def compare(x, y):
+    if int(str(x)+str(y))>int(str(y)+str(x)):
+        return -1
+    
+    if int(str(x)+str(y))<int(str(y)+str(x)):
+        return 1
+    
+    return 0
+
+# Please write your code here.
+arr.sort(key=cmp_to_key(compare))
+
+print("".join(arr))
