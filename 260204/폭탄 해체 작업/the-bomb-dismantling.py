83ce700674f1651f0dde0791bb0b68d0e2d46eb6
Lee Sung Jae <sa478@naver.com>
2026-02-04 12:34:13 +0900

    Title: 폭탄 해체 작업, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260204/폭탄 해체 작업/the-bomb-dismantling.py
+++ 260204/폭탄 해체 작업/the-bomb-dismantling.py
@@ -1,13 +1,40 @@
-N = int(input())
-bombs = [tuple(map(int, input().split())) for _ in range(N)]
+n = int(input())
+bombs = [tuple(map(int, input().split())) for _ in range(n)]
+#print(bombs)
 
-# Please write your code here.
-#bombs.sort(key=lambda x: x[1])
+# n초에서 1초까지 내려가면서
+# 현재 초~n초 중 가장 값이 큰 폭탄을 해체
+result=0
+for i in range(n-1, 0, -1):
+    #print(f"{i}번째 시간")
+    biggest_bomb=-1
+    index=-1
+    for j in range(len(bombs)):
+        if bombs[j][1]>=i and biggest_bomb<bombs[j][0]:
+            index=j
+            biggest_bomb=bombs[j][0]
+    #print(biggest_bomb)
+    if index!=-1:
+        score, time = bombs.pop(index)
+        result+=score
+    #print(bombs)
 
-# print(bombs)
+print(result)
 
-maps={}
-for score, time in bombs:
-    maps[time]=max(maps.get(time, 0), score)
 
-print(sum(list(maps.values())))
+# #2 1
+# 10 1
+# #1 2
+# 10 3
+# #4 4
+# 8 4
+# 9 5
+# 10 5
+# 3 7
+# 2 8
+
+
+
+
+
+
