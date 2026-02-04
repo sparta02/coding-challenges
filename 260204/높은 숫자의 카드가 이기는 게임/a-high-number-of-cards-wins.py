e1c36a7003cc7c79361016da4ecc6ded023cfb64
Lee Sung Jae <sa478@naver.com>
2026-02-04 11:41:34 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 1096ms, Memory: 18MB, Status: Time Limit Exceed - Codetree

--- 260204/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
+++ 260204/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -12,24 +12,40 @@ A = [ x for x in range(1, 2*N+1) if x not in B]
 # 최종 점수
 result=0
 
+# B보다 큰 숫자들 중 가장 작은 index 반환
+def search_index(target):
+    left=0
+    right=len(A)-1
+    mid_idx=9999999
+
+    while left<=right:
+        mid= (left+right)//2
+        if A[mid]>target:
+            mid_idx=min(mid_idx, mid)
+            right=mid-1
+        else:
+            left=mid+1
+    return mid_idx
+
+
 for i in range(N):
     B_card = B[i]
     #print(f"B 카드: {B_card}")
 
-    # Case 2. A가 이길 수 없는 경우
-    if max(A)<B_card:
-        A.pop(0)
-        #print("이길 수 없음")
-        #print(A)
+    A_index=search_index(B_card)
+
     # Case 1. A가 이길 수 있는 경우
-    else:
-        for A_card in A:
-            if A_card>B_card:
-                A.remove(A_card)
-                break
-        #print("이김")
-        #print(A)
+    if A_index != 9999999:
+        A.pop(A_index)
+        # print("이김")
+        # print(A)
         result+=1
+    # Case 2. A가 이길 수 없는 경우
+    else:
+        A.pop(0)
+        # print("이길 수 없음")
+        # print(A)
+        
 
 print(result)
 
