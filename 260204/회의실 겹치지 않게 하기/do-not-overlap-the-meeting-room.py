8bfe60cbf90dddeed926cbd31729194ad8e1605c
Lee Sung Jae <sa478@naver.com>
2026-02-04 12:59:48 +0900

    Title: 회의실 겹치지 않게 하기, Time: 281ms, Memory: 28MB, Status: Passed - Codetree

---
+++ 260204/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.py
@@ -0,0 +1,19 @@
+n = int(input())
+meetings = [tuple(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+
+meetings=sorted(meetings, key = lambda x:x[1])
+
+#print(meetings)
+
+meet_list=[]
+
+meet_list.append(meetings[0])
+
+for i in range(1, n):
+    if meetings[i][0]>=meet_list[-1][1]:
+        meet_list.append(meetings[i])
+#print(meet_list)
+
+print(n-len(meet_list))
