663b870b7077d933ffcb2680bcff65100fda0e6f
Lee Sung Jae <sa478@naver.com>
2026-02-04 11:54:22 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 1091ms, Memory: 21MB, Status: Time Limit Exceed - Codetree

--- 260204/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
+++ 260204/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -1,51 +1,30 @@
+import heapq
+
 N = int(input())
 B = [int(input()) for _ in range(N)]
-
+heapq.heapify(B)
 # 1 (2) (3) 4 (5) 6
 
-# 그리디 알고리즘
-# 1. 만약 A가 B를 이길 수 있다면, B 카드 보다 큰 카드 중 가장 작은 카드를 낸다
-# 2. 만약 A가 B를 이길 수 없다면, 가장 작은 카드를 낸다
+A=[]
+for x in range(1, 2*N+1):
+    if x not in B:
+        A.append(x)
 
-A = [ x for x in range(1, 2*N+1) if x not in B]
-
-# 최종 점수
 result=0
 
-# B보다 큰 숫자들 중 가장 작은 index 반환
-def search_index(target):
-    left=0
-    right=len(A)-1
-    mid_idx=9999999
+# B를 우선순위 큐로 만든 후,
+# A의 원소들을 하나씩 B의 최솟값과 비교한다
+# 만일 A의 원소가 더 크다면 +1 후 B의 최솟값 pop
+# 아니라면 다시 원소를 B에 넣는다
 
-    while left<=right:
-        mid= (left+right)//2
-        if A[mid]>target:
-            mid_idx=min(mid_idx, mid)
-            right=mid-1
-        else:
-            left=mid+1
-    return mid_idx
-
-
-for i in range(N):
-    B_card = B[i]
-    #print(f"B 카드: {B_card}")
-
-    A_index=search_index(B_card)
-
-    # Case 1. A가 이길 수 있는 경우
-    if A_index != 9999999:
-        A.pop(A_index)
-        # print("이김")
-        # print(A)
+for A_card in A:
+    B_min = heapq.heappop(B)
+    #print(A_card)
+    if A_card>B_min:
         result+=1
-    # Case 2. A가 이길 수 없는 경우
     else:
-        A.pop(0)
-        # print("이길 수 없음")
-        # print(A)
-        
+        heapq.heappush(B, B_min)
+    # print(result)
+    # print(B)
 
 print(result)
-
