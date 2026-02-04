4f1c3e308add7f37af195e073bd9bdd2c3c1a8dd
Lee Sung Jae <sa478@naver.com>
2026-02-04 12:52:37 +0900

    Title: 자연수 M/2개의 쌍, Time: 109ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 260204/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -0,0 +1,49 @@
+import heapq
+
+N = int(input())
+x, y = zip(*[tuple(map(int, input().split())) for _ in range(N)])
+x, y = list(x), list(y)
+# x는 개수, y는 값
+min_heap=y[:]
+heapq.heapify(min_heap)
+max_heap=[ -num for num in y]
+heapq.heapify(max_heap)
+
+result=0
+
+maps={}
+for i in range(N):
+    maps[y[i]]=x[i]
+
+left=heapq.heappop(min_heap)
+left_count=maps[left]
+right=-heapq.heappop(max_heap)
+right_count=maps[right]
+life=int(sum(x)/2)
+
+while life:
+    # print(f"현재 life:{life}")
+    # print(f"left: {left}, {left_count}개")
+    # print(f"right: {right}, {right_count}개")
+    # 작은 수가 더 많은 경우
+    if left_count>right_count:
+        result=max(left+right, result)
+        life-=right_count
+        left_count-=right_count
+        right=-heapq.heappop(max_heap)
+    # 큰 수가 더 많은 경우
+    elif left_count<right_count:
+        result=max(left+right, result)
+        life-=left_count
+        right_count-=left_count
+        left=heapq.heappop(min_heap)
+    # 똑같은 경우
+    else:
+        result=max(left+right, result)
+        life-=left_count
+        left=heapq.heappop(min_heap)
+        right=-heapq.heappop(max_heap)
+    # print(f"left: {left}, {left_count}개")
+    # print(f"right: {right}, {right_count}개")
+
+print(result)
