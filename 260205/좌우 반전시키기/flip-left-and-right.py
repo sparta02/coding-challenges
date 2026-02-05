8f878f7bcd48dd02af979a2274ccebbff6125ee2
Lee Sung Jae <sa478@naver.com>
2026-02-05 11:49:30 +0900

    Title: 좌우 반전시키기, Time: 76ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 260205/좌우 반전시키기/flip-left-and-right.py
@@ -0,0 +1,21 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+
+def change(num):
+    for i in range(num-1, num+2):
+        if 0<=num<n:
+            if arr[i]==0:
+                arr[i]=1
+            else:
+                arr[i]=0
+
+result=0
+for i in range(n-1):
+    if arr[i]==0:
+        change(i+1)
+        result+=1
+    #print(arr)
+
+print(result)
