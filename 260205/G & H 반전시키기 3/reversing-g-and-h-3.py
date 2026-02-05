f35064df68442511f839a84fba8473924a7f330c
Lee Sung Jae <sa478@naver.com>
2026-02-05 12:08:54 +0900

    Title: G & H 반전시키기 3, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260205/G & H 반전시키기 3/reversing-g-and-h-3.py
+++ 260205/G & H 반전시키기 3/reversing-g-and-h-3.py
@@ -8,8 +8,26 @@ compare=[]
 
 for i in range(N):
     if a[i]==b[i]:
-        compare[i]=0
+        compare.append(0)
     else:
-        compare[i]=1
+        compare.append(1)
 
-print(compare)
+result=0
+compare2=[]
+temp=0
+for i in range(N):
+    if compare[i]==1:
+        temp+=1
+    elif compare[i]==0 and temp!=0:
+        compare2.append(temp)
+        temp=0
+
+# print(compare)
+# print(compare2)
+
+for i in range(len(compare2)):
+    if compare2[i]<=4:
+        result+=1
+    else:
+        result+=int((compare2[i]+3)//4)
+print(result)
