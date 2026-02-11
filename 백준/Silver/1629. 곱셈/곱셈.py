deca0d382533aad47cb82213be5d9b2ec2775db1
Lee Sung Jae <sa478@naver.com>
2026-02-11 13:24:41 +0900

    [Silver I] Title: 곱셈, Time: 76 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,17 +1,3 @@
 a, b, c=map(int, input().split())
 
-maps={}
-maps[1]=a%c
-
-def calc(num):
-    if num in maps:
-        return maps[num]
-    
-    
-    temp=int(num/2)
-    calc(temp)
-    calc(num-temp)
-    maps[num]=(maps[temp]*maps[num-temp])%c
-
-calc(b)
-print(maps[b])
+print(pow(a,b,c))
