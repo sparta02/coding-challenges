8fc698a55cd6fb6537faf801438115bc66117225
Lee Sung Jae <sa478@naver.com>
2026-02-18 14:25:24 +0900

    Title: 가장 오래 걸리는 학생 2, Time: 79ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 260218/가장 오래 걸리는 학생 2/longest-student-2.py
@@ -0,0 +1,29 @@
+import heapq
+n, m = map(int, input().split())
+edges=[[] for _ in range(m)]
+
+for _ in range(m):
+    s, e, d = map(int, input().split())
+    edges[e].append((s,d))
+
+# print(edges)
+
+INF=10**6
+dist=[INF]*(n+1)
+dist[n]=0
+pq=[]
+
+heapq.heappush(pq, (0, n))
+
+while pq:
+    curr_dist, curr_node= heapq.heappop(pq)
+
+    if dist[curr_node]<curr_dist:
+        continue
+    for next_node, next_weight in edges[curr_node]:
+        weight=curr_dist+next_weight
+
+        if dist[next_node]>weight:
+            dist[next_node]=weight
+            heapq.heappush(pq, (weight, next_node))
+print(max(dist[1:]))
