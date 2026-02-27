f21c03f0a913d396944dbf70bc29427c51df5d38
Lee Sung Jae <sa478@naver.com>
2026-02-27 15:48:40 +0900

    Title: 정점 연결하기, Time: 130ms, Memory: 23MB, Status: Runtime Error - Codetree

---
+++ 260227/정점 연결하기/connect-vertices.py
@@ -0,0 +1,35 @@
+n = int(input())
+edges = [tuple(map(int, input().split())) for _ in range(n - 2)]
+
+# Please write your code here.
+roots=set()
+parent= [ i for i in range(n+1)]
+
+def find(x):
+    if parent[x]!=x:
+        parent[x]=find(parent[x])
+    return parent[x]
+
+def union(a, b):
+    A=find(a)
+    B=find(b)
+    min_num, max_num=min(A,B), max(A,B)
+
+    parent[max_num]=min_num
+    if min_num not in roots:
+        roots.add(min_num)
+    if max_num in roots:
+        roots.pop(max_num)
+
+for a, b in edges:
+    union(a, b)
+
+# result=set()
+# i=1
+# while(len(result)<2):
+#     result.add(find(1))
+#     i+=1
+# result=list(result).sort()
+roots=list(roots)
+roots.sort()
+print(*roots)
