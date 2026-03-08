48d930c9e3ff76fc4497368bfb4c52572e20497c
Lee Sung Jae <sa478@naver.com>
2026-03-08 15:52:28 +0900

    Title: 크기 비교 3, Time: 50ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 260308/크기 비교 3/size-comparison-3.py
@@ -0,0 +1,26 @@
+from collections import deque
+n, m = map(int, input().split())
+edges = [ [] for _ in range(n+1)]
+indegree=[0]*(n+1)
+queue=deque()
+
+for _ in range(m):
+    s, e= map(int, input().split())
+    edges[s].append(e)
+    edges[s].sort()
+
+    indegree[e]+=1
+
+for i in range(1, n+1):
+    if indegree[i]==0:
+        queue.append(i)
+
+while queue:
+    curr=queue.popleft()
+    print(curr, end=" ")
+
+    for next in edges[curr]:
+        indegree[next]-=1
+
+        if indegree[next]==0:
+            queue.append(next)
