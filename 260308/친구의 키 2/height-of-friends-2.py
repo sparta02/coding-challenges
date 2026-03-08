cfbadc5ae19d29ce0b4e7ff2340bc9b5d7df76b3
Lee Sung Jae <sa478@naver.com>
2026-03-08 15:42:25 +0900

    Title: 친구의 키 2, Time: 54ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260308/친구의 키 2/height-of-friends-2.py
@@ -0,0 +1,32 @@
+from collections import deque
+import sys
+n, m = map(int, input().split())
+edges=[[] for _ in range(n+1)]
+indegree= [0] * (n+1)
+
+for _ in range(m):
+    s, e= map(int, input().split())
+    edges[s].append(e)
+
+    indegree[e]+=1
+
+
+queue=deque()
+for i in range(1, n+1):
+    if indegree[i]==0:
+        queue.append(i)
+cnt=0
+while queue:
+    curr=queue.popleft()
+
+    for next in edges[curr]:
+        indegree[next]-=1
+        cnt+=1
+        
+        if indegree[next]==0:
+            queue.append(next)
+if cnt==n:
+    print('Consistent')
+else:
+    print('Ionsistent')
+
