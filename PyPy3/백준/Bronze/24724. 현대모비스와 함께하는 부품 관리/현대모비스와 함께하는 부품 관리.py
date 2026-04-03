f0f45e1a07c62e553c9e0b27b87a3998e31b8414
Lee Sung Jae <sa478@naver.com>
2026-04-03 19:10:37 +0900

    [Bronze IV] Title: 현대모비스와 함께하는 부품 관리, Time: 192 ms, Memory: 120912 KB -BaekjoonHub

---
+++ PyPy3/백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.py
@@ -0,0 +1,9 @@
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    a, b = map(int, input().split())
+    arr=[list(map(int, input().split())) for _ in range(n)]
+
+for i in range(1, t+1):
+    print('Material Management '+str(i))
+    print('Classification ---- End!')
