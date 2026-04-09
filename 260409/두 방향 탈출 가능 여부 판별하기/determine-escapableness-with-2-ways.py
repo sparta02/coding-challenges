a878c011aed62f61209b30cb6f4f9a79e814fc66
Lee Sung Jae <sa478@naver.com>
2026-04-09 18:38:30 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260409/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 260409/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -5,7 +5,8 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 # Please write your code here.
 stack=[]
 stack.append((0,0))
-
+visited=[ [0]*(m) for _ in range(n)]
+visited[0][0]=1
 while stack:
     curr_x, curr_y=stack.pop()
     if (curr_x, curr_y) == (n-1, m-1):
@@ -17,7 +18,8 @@ while stack:
         next_x=curr_x+dx[i]
         next_y=curr_y+dy[i]
         if 0<=next_x<n and 0<=next_y<m:
-            if grid[next_x][next_y]==1:
+            if grid[next_x][next_y]==1 and visited[next_x][next_y]==0:
+                visited[next_x][next_y]=1
                 stack.append((next_x, next_y))
 
 
