87d63f4249475fe16670d9800607d66a6a05a88e
Lee Sung Jae <sa478@naver.com>
2026-04-13 10:58:59 +0900

    Title: 정수 분배하기, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260413/정수 분배하기/distributing-integers.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+arr = [int(input()) for _ in range(n)]
+
+
+
+left =0
+right=min(arr)
+max_idx=-1
+
+def check_num(num):
+    temp=0
+    for i in arr:
+        temp+=i//num
+    return temp
+
+while left<=right:
+    mid=(left+right)//2
+    if check_num(mid)>=m:
+        max_idx=max(max_idx, mid)
+        left=mid+1
+    else:
+        right=mid-1
+
+print(max_idx)
