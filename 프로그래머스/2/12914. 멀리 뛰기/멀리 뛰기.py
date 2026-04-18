8efd8e10f1bf41bfedcd339d89c1490119119e9e
Lee Sung Jae <sa478@naver.com>
2026-04-18 16:33:47 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.37 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,13 @@
+def solution(n):
+    if n==1:
+        return 1
+    elif n==2:
+        return 2
+    answer = 0
+    dp=[0]*(n+1)
+    dp[0]=1
+    dp[1]=1
+    dp[2]=2
+    for i in range(3, n+1):
+        dp[i]=(dp[i-1]+dp[i-2])%1234567
+    return dp[-1]
