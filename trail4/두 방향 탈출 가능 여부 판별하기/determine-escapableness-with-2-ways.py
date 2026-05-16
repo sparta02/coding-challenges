fbfea8146981db5efa6b1b71863823451c8a851a
Lee Sung Jae <sa478@naver.com>
2026-05-16 12:48:14 +0900

    [Passed] 두 방향 탈출 가능 여부 판별하기 (54ms, 17MB)

---
+++ trail4/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,27 @@
+import sys
+
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+
+visited=[ [0]*m for _ in range(n)]
+
+stack=[(0,0)]
+visited[0][0]=1
+while stack:
+    curr_x, curr_y=stack.pop()
+    
+    if (curr_x, curr_y)==(n-1, m-1):
+        print(1)
+        sys.exit()
+
+    dx=[1,0]
+    dy=[0,1]
+    for i in range(2):
+        next_x, next_y=curr_x+dx[i], curr_y+dy[i]
+        if 0<=next_x<n and 0<=next_y<m and visited[next_x][next_y]==0 and grid[next_x][next_y]==1:
+            visited[next_x][next_y]=1
+            stack.append((next_x, next_y))
+
+print(0)
